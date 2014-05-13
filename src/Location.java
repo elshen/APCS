d7e88f72327addd3a0eaa37efdc110534c6e719b
elshen <flyingcitrusfruits19@gmail.com>
2014-05-13 13:30:26 -0700

    edited player and location classes

--- src/Location.java
+++ src/Location.java
@@ -1,3 +1,13 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.*;
+
+import java.util.Scanner;
+import java.util.Random;
 
 public class Location 
 {
--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,31 @@
+import java.util.ArrayList;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class Player {
+import javax.swing.*;
 
+import java.util.Scanner;
+import java.util.Random;
+
+public class Player 
+{
+	private ArrayList<Location> listOfLocs;
+	private Color color;
+	
+	public Player(Color color)
+	{
+		listOfLocs = new ArrayList<Location>();
+		this.color = color;
+	}
+	public ArrayList<Location> getLocs()
+	{
+		return listOfLocs;
+	}
+	public void addNewLoc()
+	{
+		
+	}
 }
