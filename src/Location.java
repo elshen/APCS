0d49623d92d0145b9b6cb01366fa5c35b78343e8
mastern123 <matthew.stern@menloschool.org>
2014-05-13 13:47:57 -0700

    Update Location.java

--- src/Location.java
+++ src/Location.java
@@ -13,7 +13,7 @@ public class Location
 {
   private int x;
   private int y;
-  final int RADIUS = 5;
+  final int LRADIUS = 5;
   private Color c;
   
   public Location(int dx, int dy, Color dcolor)
