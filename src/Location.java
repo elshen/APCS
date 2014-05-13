cf9dcd460aadee170083c0b17081fb35b8dca142
mastern123 <matthew.stern@menloschool.org>
2014-05-13 13:09:41 -0700

    Update Location.java

--- src/Location.java
+++ src/Location.java
@@ -1,4 +1,16 @@
 
-public class Location {
-
+public class Location 
+{
+  private int x;
+  private int y;
+  final int RADIUS = 5;
+  private Color c;
+  
+  public Location(int dx, int dy, Color dcolor)
+  {
+    x = dx;
+    y = dy
+    c = dcolor
+  }
+  
 }
