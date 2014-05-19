960facbba79029dd8864cb804f69776078b76133
jc10333 <jcardozo3@gmail.com>
2014-05-19 11:24:30 -0700

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -10,127 +10,78 @@ import javax.swing.*;
 import java.util.Scanner;
 import java.util.Random;
 
-public class Player 
+public class Player
 {
-	private ArrayList<Location> listOfLocs;
 	public static final int WINDOW_WIDTH = 1000;
 	public static final int WINDOW_HEIGHT = 800;
-	private Location lastLoc;
 	private Color color;
-	private int x;
-	private int y;
-	private int dx;
-	private int dy;
-	public final static int RADIUSP = 5;
-	public final static int V = 6;
-	
-	public Player(Color color, int xIn, int yIn)
+	private double x;
+	private double y;
+	private double dx;
+	private double dy;
+	public final static double RADIUSP = 3.5;
+
+	public Player(Color color, double xIn, double yIn, double dxIn, double dyIn)
 	{
-		listOfLocs = new ArrayList<Location>();
+		
 		this.color = color;
 		x = xIn;
 		y = yIn;
+		dx = dxIn;
+		dy = dyIn;
 	}
-	public ArrayList<Location> getLocs()
-	{
-		return listOfLocs;
-	}
-	public void addNewLoc()
-	{
-		Location loc = new Location(x, y, color);
-		listOfLocs.add(loc);
-		lastLoc = listOfLocs.get(listOfLocs.size() - 2);
-	}
-	
-	public boolean checkCollisions(Player otherPlayer)
-	{
-		if( x >= WINDOW_WIDTH || x <= 0)
-			return true;
-		
-		if (y >= WINDOW_HEIGHT|| y <= 0)
-			return true;
-			
-		for(Location loc: listOfLocs)
-		{
-			if(checkHit(loc))
-				return true;
-		}
-		
-		for(Location loc: otherPlayer.getLocs())
-		{
-			if(checkHit(loc))
-				return true;
-		}
-		return false;
-	}
-	
-	public boolean checkHit(Location loc)
-	{
-		double xDistance = loc.getX() - x;
-		double yDistance = loc.getY() - y;
 
-		if ((xDistance * xDistance) + (yDistance * yDistance) <= (RADIUSP + Location.LRADIUS) * (RADIUSP + Location.LRADIUS)) // finds if player is touching a location
-		{
-			if (loc.compareTo(lastLoc) == 0) // check to make sure that its not the most recently dropped location
-			{
-				return false;
-			}
-			else
-			{
-				return true;
-			}
-		}
-		else 
-		{
-			return false;
-		}
-	}
-	
 	public void move()
 	{
 		x = x + dx;
 		y = y + dy;
 	}
-	
-	public int getX()
+
+	public double getX()
 	{
 		return x;
 	}
-	
-	public int getY()
+
+	public double getY()
 	{
 		return y;
 	}
-	
-	public void setX( int newX)
+
+	public void setX( double newX)
 	{
 		x = newX;
 	}
-	
-	public void setY( int newY)
+
+	public void setY( double newY)
 	{
 		y = newY;
 	}
-	
-	public int getDX()
+
+	public double getDX()
 	{
 		return dx;
 	}
-	
-	public int getDY()
+
+	public double getDY()
 	{
 		return dy;
 	}
-	
-	public void setDX( int newDX)
+
+	public void setDX( double newDX)
 	{
 		dx = newDX;
 	}
-	
-	public void setDY( int newDY)
+
+	public void setDY( double newDY)
 	{
 		dy = newDY;
 	}
 	
-	
+	public void draw(Graphics g)
+	{
+		g.setColor(Color.red);
+		g.fillOval((int)(x-RADIUSP), (int)(y - RADIUSP), (int)(RADIUSP*2), (int)(RADIUSP*2));
+	}
+
+
 }
