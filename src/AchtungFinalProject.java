8faa2a152f2802be98929be27c48a2a12d03d7b0
mastern123 <matthew.stern@menloschool.org>
2014-05-22 09:44:43 -0700

    Update AchtungFinalProject.java

--- src/AchtungFinalProject.java
+++ src/AchtungFinalProject.java
@@ -15,15 +15,13 @@ import java.util.HashSet;
 
 public class AchtungFinalProject extends JFrame implements ActionListener, KeyListener
 {
-	static final int WINDOW_WIDTH = 1000;
-	static final int WINDOW_HEIGHT = 800;
-	static final int TOP_OF_WINDOW = 22;	// Top of the visible window
+	public static final int WINDOW_WIDTH = 1000;
+	public static final int WINDOW_HEIGHT = 800;
+	public static final int TOP_OF_WINDOW = 22;	// Top of the visible window
 	public static final int DELAY_IN_MILLISEC = 30;  // Time delay between updates
 	public static final double MAX_VELOCITY = 3;
-	public static final int PLAYERONELEFT = KeyEvent.VK_LEFT;
-	public static final int PLAYERONERIGHT = KeyEvent.VK_RIGHT;
-	public static final int PLAYERTWOLEFT = KeyEvent.VK_A;
-	public static final int PLAYERTWORIGHT = KeyEvent.VK_D;
+	public static final int FONT_SIZE = 30;
+
 	public static double multiple = .1;
 	public static double dx1 = Math.sqrt(MAX_VELOCITY*MAX_VELOCITY/2);
 	public static double dy1 = Math.sqrt(MAX_VELOCITY*MAX_VELOCITY/2);
@@ -31,6 +29,9 @@ public class AchtungFinalProject extends JFrame implements ActionListener, KeyLi
 	public static double dy2 = Math.sqrt(MAX_VELOCITY*MAX_VELOCITY/2);
 
 	public static boolean dead = false;
+	public static boolean gameStarted = false;
+	public static boolean playerOneWins = false;
+	public static boolean playerTwoWins = false;
 
 	public static boolean[][] pixelTemplate = 
 		{{false, false, false, false ,false, false, false},
@@ -92,128 +93,141 @@ public class AchtungFinalProject extends JFrame implements ActionListener, KeyLi
 	 */
 	public void actionPerformed(ActionEvent e)
 	{	
-
-		if(keysPressed.contains(PLAYERONELEFT))
+		if(keysPressed.contains(KeyEvent.VK_ENTER))
 		{
-			//left player one
-			double temp = dx1;
-			dx1 = dx1 + multiple * dy1;
-			dy1 = dy1 - multiple * temp;
-			double currentVelocity = Math.sqrt(dx1*dx1 + dy1*dy1);
-			dx1 = dx1 / currentVelocity * MAX_VELOCITY;
-			dy1 = dy1 / currentVelocity * MAX_VELOCITY;
-
-			p1.setDX(dx1);
-			p1.setDY(dy1);
-
+			gameStarted = true;
 		}
-		if(keysPressed.contains(PLAYERONERIGHT))
+		if(gameStarted)
 		{
-			double temp = dx1;
-			dx1 = dx1 - multiple * dy1;
-			dy1 = dy1 + multiple * temp;
-			double currentVelocity = Math.sqrt(dx1*dx1 + dy1*dy1);
-			dx1 = dx1 / currentVelocity * MAX_VELOCITY;
-			dy1 = dy1 / currentVelocity * MAX_VELOCITY;
 
-			p1.setDX(dx1);
-			p1.setDY(dy1);
-		}
-
-
-		if(keysPressed.contains(PLAYERTWOLEFT))
-		{
-			double temp = dx2;
-			dx2 = dx2 + multiple * dy2;
-			dy2 = dy2 - multiple * temp;
-			double currentVelocity = Math.sqrt(dx2*dx2 + dy2*dy2);
-			dx2 = dx2 / currentVelocity * MAX_VELOCITY;
-			dy2 = dy2 / currentVelocity * MAX_VELOCITY;
-
-			p2.setDX(dx2);
-			p2.setDY(dy2);
-		}
-		if(keysPressed.contains(PLAYERTWORIGHT))
-		{
-			double temp = dx2;
-			dx2 = dx2 - multiple * dy2;
-			dy2 = dy2 + multiple * temp;
-			double currentVelocity = Math.sqrt(dx2*dx2 + dy2*dy2);
-			dx2 = dx2 / currentVelocity * MAX_VELOCITY;
-			dy2 = dy2 / currentVelocity * MAX_VELOCITY;
-
-			p2.setDX(dx2);
-			p2.setDY(dy2);
-		}
-
-		if(!dead)
-		{
-			p1.move();
-			p2.move();
-
-			if(p1.getX() > 0 && p1.getY() > 0 && p1.getX() < WINDOW_WIDTH && p1.getY() < WINDOW_HEIGHT)
+			if(keysPressed.contains(KeyEvent.VK_LEFT))
 			{
-				if(screenPixels[(int)p1.getX()][(int)p1.getY()])
-				{
-					System.out.println("dead");
-					dead = true;
-				}
+				//left player one
+				double temp = dx1;
+				dx1 = dx1 + multiple * dy1;
+				dy1 = dy1 - multiple * temp;
+				double currentVelocity = Math.sqrt(dx1*dx1 + dy1*dy1);
+				dx1 = dx1 / currentVelocity * MAX_VELOCITY;
+				dy1 = dy1 / currentVelocity * MAX_VELOCITY;
 
-				int currentX = (int)(p1.getX());
-				int currentY = (int)(p1.getY());
+				p1.setDX(dx1);
+				p1.setDY(dy1);
 
-				for(int x = currentX - 3; x <= currentX + 3; x++)
+			}
+			if(keysPressed.contains(KeyEvent.VK_RIGHT))
+			{
+				double temp = dx1;
+				dx1 = dx1 - multiple * dy1;
+				dy1 = dy1 + multiple * temp;
+				double currentVelocity = Math.sqrt(dx1*dx1 + dy1*dy1);
+				dx1 = dx1 / currentVelocity * MAX_VELOCITY;
+				dy1 = dy1 / currentVelocity * MAX_VELOCITY;
+
+				p1.setDX(dx1);
+				p1.setDY(dy1);
+			}
+
+
+			if(keysPressed.contains(KeyEvent.VK_A))
+			{
+				double temp = dx2;
+				dx2 = dx2 + multiple * dy2;
+				dy2 = dy2 - multiple * temp;
+				double currentVelocity = Math.sqrt(dx2*dx2 + dy2*dy2);
+				dx2 = dx2 / currentVelocity * MAX_VELOCITY;
+				dy2 = dy2 / currentVelocity * MAX_VELOCITY;
+
+				p2.setDX(dx2);
+				p2.setDY(dy2);
+				//System.out.println("vk1");
+			}
+			if(keysPressed.contains(KeyEvent.VK_D))
+			{
+				double temp = dx2;
+				dx2 = dx2 - multiple * dy2;
+				dy2 = dy2 + multiple * temp;
+				double currentVelocity = Math.sqrt(dx2*dx2 + dy2*dy2);
+				dx2 = dx2 / currentVelocity * MAX_VELOCITY;
+				dy2 = dy2 / currentVelocity * MAX_VELOCITY;
+
+				p2.setDX(dx2);
+				p2.setDY(dy2);
+				//System.out.println("vk2");
+			}
+
+			if(!dead)
+			{
+				p1.move();
+				p2.move();
+
+				if(p1.getX() > 0 && p1.getY() > 0 && p1.getX() < WINDOW_WIDTH && p1.getY() < WINDOW_HEIGHT)
 				{
-					for(int y = currentY - 3; y <= currentY + 3; y++)
+					if(screenPixels[(int)p1.getX()][(int)p1.getY()])
 					{
-						if(x > 0 && y > 0 && x < WINDOW_WIDTH && y < WINDOW_HEIGHT)
+						//System.out.println("dead");
+						dead = true;
+						playerTwoWins = true;
+					}
+
+					int currentX = (int)(p1.getX());
+					int currentY = (int)(p1.getY());
+
+					for(int x = currentX - 3; x <= currentX + 3; x++)
+					{
+						for(int y = currentY - 3; y <= currentY + 3; y++)
 						{
-							if(pixelTemplate[x+3-currentX][y+3-currentY])
+							if(x > 0 && y > 0 && x < WINDOW_WIDTH && y < WINDOW_HEIGHT)
 							{
-								screenPixels[x][y] = true;
+								if(pixelTemplate[x+3-currentX][y+3-currentY])
+								{
+									screenPixels[x][y] = true;
+								}
 							}
 						}
 					}
 				}
-			}
-			else
-			{
-				dead = true;
-			}
-			//System.out.println(Math.sqrt(dx*dx + dy*dy));
-			if(p2.getX() > 0 && p2.getY() > 0 && p2.getX() < WINDOW_WIDTH && p2.getY() < WINDOW_HEIGHT)
-			{
-				if(screenPixels[(int)p2.getX()][(int)p2.getY()])
+				else
 				{
-					System.out.println("dead");
 					dead = true;
+					playerTwoWins = true;
 				}
-
-				int currentX = (int)(p2.getX());
-				int currentY = (int)(p2.getY());
-
-				for(int x = currentX - 3; x <= currentX + 3; x++)
+				//System.out.println(Math.sqrt(dx*dx + dy*dy));
+				if(p2.getX() > 0 && p2.getY() > 0 && p2.getX() < WINDOW_WIDTH && p2.getY() < WINDOW_HEIGHT)
 				{
-					for(int y = currentY - 3; y <= currentY + 3; y++)
+					if(screenPixels[(int)p2.getX()][(int)p2.getY()])
 					{
-						if(x > 0 && y > 0 && x < WINDOW_WIDTH && y < WINDOW_HEIGHT)
+						//System.out.println("dead");
+						dead = true;
+						playerOneWins = true;
+					}
+
+					int currentX = (int)(p2.getX());
+					int currentY = (int)(p2.getY());
+
+					for(int x = currentX - 3; x <= currentX + 3; x++)
+					{
+						for(int y = currentY - 3; y <= currentY + 3; y++)
 						{
-							if(pixelTemplate[x+3-currentX][y+3-currentY])
+							if(x > 0 && y > 0 && x < WINDOW_WIDTH && y < WINDOW_HEIGHT)
 							{
-								screenPixels[x][y] = true;
+								if(pixelTemplate[x+3-currentX][y+3-currentY])
+								{
+									screenPixels[x][y] = true;
+								}
 							}
 						}
 					}
 				}
+				else
+				{
+					dead = true;
+					playerOneWins = true;
+				}
 			}
-			else
-			{
-				dead = true;
-			}
-			// Update the window.
-			repaint();
+			time++;
 		}
-		time++;
+		// Update the window.
+		repaint();
 	}
 
 	/**
@@ -247,15 +261,39 @@ public class AchtungFinalProject extends JFrame implements ActionListener, KeyLi
 
 	public void paint(Graphics g)
 	{
-		// Clear the window.
-		if(time==0)
+		if(gameStarted)
+		{
+			// Clear the window.
+			if(time==1)
+			{
+				g.setColor(Color.black);
+				g.fillRect(0, TOP_OF_WINDOW, WINDOW_WIDTH, WINDOW_HEIGHT - TOP_OF_WINDOW);
+			}
+			p1.draw(g);
+			p2.draw(g);
+			if(playerOneWins)
+			{
+				g.setColor(Color.white);
+				g.setFont(new Font("Serif", Font.PLAIN, FONT_SIZE));
+				g.drawString("Player One Wins!", 0, TOP_OF_WINDOW + FONT_SIZE);
+			}
+			if(playerTwoWins)
+			{
+				g.setColor(Color.white);
+				g.setFont(new Font("Serif", Font.PLAIN, FONT_SIZE));
+				g.drawString("Player Two Wins!", 0, TOP_OF_WINDOW + FONT_SIZE);
+			}
+		}
+		else
 		{
 			g.setColor(Color.black);
 			g.fillRect(0, TOP_OF_WINDOW, WINDOW_WIDTH, WINDOW_HEIGHT - TOP_OF_WINDOW);
+			g.setColor(Color.white);
+			g.setFont(new Font("Serif", Font.PLAIN, FONT_SIZE));
+			g.drawString("PRESS ENTER TO START", 0, TOP_OF_WINDOW + FONT_SIZE);
 		}
-		p1.draw(g);
-		p2.draw(g);
 
 	}
-	
+
 }
+
